1e4c946b6d5530b92d6492fc9c97a8630a4cad29
mkvdev <valentin.c.buzduga@gmail.com>
2016-12-01 00:58:41 +0200

    Add files via upload

---
+++ JScelsius.js
@@ -0,0 +1,10 @@
+function multiplyByNineFifths(celsius) {
+  return celsius * (9/5);
+}
+
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+}
+
+console.log('The temperature is ' + getFahrenheit(15) + '°F');
+// Output: The temperature is 59°F
